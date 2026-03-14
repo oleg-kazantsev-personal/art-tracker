cd95c5f690427d6dc5034b49bcd39a042873451d
Oleg Kazantsev <ok020373@gmail.com>
2026-03-15 00:37:40 +1300

    HTTPS has been set up

--- ArtTracker/Program.cs
+++ ArtTracker/Program.cs
@@ -3,6 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure Kestrel.
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5064);
+
+    options.ListenAnyIP(7244, listenOptions =>
+    {
+        listenOptions.UseHttps("./cert/mini256.local+3.p12", "changeit");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
